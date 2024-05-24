b231a2988a95208bd2c8d3bf41fa9ba17892b27d
Yannik Suedbeck <yannik.suedbeck@yahoo.de>
2024-05-24 11:53:54 +0200

    added grid layout support

--- main/quantum_ui.c
+++ main/quantum_ui.c
@@ -8,45 +8,53 @@
 
 #include "lvgl.h"
 
-// static void draw_event_cb(lv_event_t *e)
-// {
-//     lv_obj_draw_part_dsc_t *dsc = lv_event_get_draw_part_dsc(e);
-//     if (dsc->part == LV_PART_ITEMS) {
-//         lv_obj_t *obj = lv_event_get_target(e);
-//         lv_chart_series_t *ser = lv_chart_get_series_next(obj, NULL);
-//         uint32_t cnt = lv_chart_get_point_count(obj);
-//         /*Make older value more transparent*/
-//         dsc->rect_dsc->bg_opa = (LV_OPA_COVER *  dsc->id) / (cnt - 1);
-
-//         /*Make smaller values blue, higher values red*/
-//         lv_coord_t *x_array = lv_chart_get_x_array(obj, ser);
-//         lv_coord_t *y_array = lv_chart_get_y_array(obj, ser);
-//         /*dsc->id is the tells drawing order, but we need the ID of the point being drawn.*/
-//         uint32_t start_point = lv_chart_get_x_start_point(obj, ser);
-//         uint32_t p_act = (start_point + dsc->id) % cnt; /*Consider start point to get the index of the array*/
-//         lv_opa_t x_opa = (x_array[p_act] * LV_OPA_50) / 200;
-//         lv_opa_t y_opa = (y_array[p_act] * LV_OPA_50) / 1000;
-
-//         dsc->rect_dsc->bg_color = lv_color_mix(lv_palette_main(LV_PALETTE_RED),
-//                                                lv_palette_main(LV_PALETTE_BLUE),
-//                                                x_opa + y_opa);
-//     }
-// }
-
-// static void add_data(lv_timer_t *timer)
-// {
-//     lv_obj_t *chart = timer->user_data;
-//     lv_chart_set_next_value2(chart, lv_chart_get_series_next(chart, NULL), lv_rand(0, 200), lv_rand(0, 1000));
-// }
+// The pixel number in horizontal and vertical
+#define LCD_H_RES 800
+#define LCD_V_RES 480
 
 void quantum_ui(lv_disp_t *disp)
 {
-      /*Change the active screen's background color*/
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57), LV_PART_MAIN);
 
-    /*Create a white label, set its text and align it to the center*/
-    lv_obj_t * label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "Hello world");
-    lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    static lv_coord_t col_dsc[] = {LCD_H_RES / 2 - 15, LCD_H_RES / 2 - 15, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc[] = {LCD_V_RES / 2 - 15, LCD_V_RES / 2 - 15, LV_GRID_TEMPLATE_LAST};
+
+    /*Create a container with grid*/
+    lv_obj_t *background = lv_obj_create(lv_scr_act());
+    lv_obj_set_style_grid_column_dsc_array(background, col_dsc, 0);
+    lv_obj_set_style_grid_row_dsc_array(background, row_dsc, 0);
+    lv_obj_set_size(background, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_center(background);
+    lv_obj_set_layout(background, LV_LAYOUT_GRID);
+
+    // lv_obj_add_flag(background, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_clear_flag(lv_scr_act(), LV_OBJ_FLAG_SCROLLABLE);
+
+    lv_obj_t *dice_btn = lv_obj_create(background);
+    /*Stretch the cell horizontally and vertically too
+     *Set span to 1 to make the cell 1 column/row sized*/
+    lv_obj_set_grid_cell(dice_btn, LV_GRID_ALIGN_STRETCH, 0, 1,
+                         LV_GRID_ALIGN_STRETCH, 0, 2);
+
+    lv_obj_t *dice_btn2 = lv_obj_create(background);
+
+    lv_obj_set_grid_cell(dice_btn2, LV_GRID_ALIGN_STRETCH, 1, 1,
+                         LV_GRID_ALIGN_STRETCH, 0, 1);
+
+    lv_obj_t *dice_btn4 = lv_obj_create(background);
+    // /*Stretch the cell horizontally and vertically too
+    //  *Set span to 1 to make the cell 1 column/row sized*/
+    lv_obj_set_grid_cell(dice_btn4, LV_GRID_ALIGN_STRETCH, 1, 1,
+                         LV_GRID_ALIGN_STRETCH, 1, 1);
+
+    lv_obj_t *btn_text_label = lv_label_create(dice_btn);
+    lv_obj_t *btn2_text_label = lv_label_create(dice_btn2);
+    lv_obj_t *btn4_text_label = lv_label_create(dice_btn4);
+
+    lv_label_set_text(btn_text_label, "btn1");
+    lv_label_set_text(btn2_text_label, "btn2");
+    lv_label_set_text(btn4_text_label, "btn4");
+
+    lv_obj_center(btn_text_label);
+    lv_obj_center(btn2_text_label);
+    lv_obj_center(btn4_text_label);
 }
